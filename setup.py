13cac6d91dde1031f5b52d5f59d7d898bac4240c
Timo Stollenwerk <contact@timostollenwerk.net>
2013-09-18 12:18:12 +0200

    Rename changes and readme to follow best practice.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ version = '1.0.1'
 setup(name='plone.app.uuid',
       version=version,
       description="Plone integration for the basic plone.uuid package",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "CHANGES.rst")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Framework :: Plone",
