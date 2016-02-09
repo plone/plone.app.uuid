c6c733b45dec2396eafddf00476a52614585bb4f
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:02:29 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,41 +1,46 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.1.1.dev0'
 
-setup(name='plone.app.uuid',
-      version=version,
-      description="Plone integration for the basic plone.uuid package",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-      ],
-      keywords='plone uuid',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='http://plone.org',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.uuid',
-          'plone.indexer',
-          'zope.publisher',
-          'zope.interface',
-      ],
-      extras_require={
-          'test': [
-              'plone.dexterity',
-              'plone.app.testing',
-              'Products.Archetypes >= 1.7'
-          ]
-      },
-      entry_points="""
-      """,
-      )
+setup(
+    name='plone.app.uuid',
+    version=version,
+    description="Plone integration for the basic plone.uuid package",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2.6',
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone uuid',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='http://plone.org',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.uuid',
+        'plone.indexer',
+        'zope.publisher',
+        'zope.interface',
+    ],
+    extras_require={
+        'test': [
+            'plone.dexterity',
+            'plone.app.testing',
+            'Products.Archetypes >= 1.7'
+        ]
+    },
+    entry_points="""
+    """,
+    )
