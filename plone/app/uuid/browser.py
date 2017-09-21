19ebcb801d45dddcb5eae5e3976010cb32c3717d
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-09-21 22:55:19 +0300

    get the object using the unrestricted methods, attempt to 'fix' the request

--- plone/app/uuid/browser.py
+++ plone/app/uuid/browser.py
@@ -6,8 +6,9 @@ from zope.publisher.browser import BrowserView
 from zope.publisher.interfaces import IPublishTraverse
 from zope.publisher.interfaces import NotFound
 from zope.location.interfaces import LocationError
-from zope.traversing.namespace import SimpleHandler
+from zope.traversing.interfaces import ITraversable
 from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
 
 
 @implementer(IPublishTraverse)
@@ -36,14 +37,37 @@ class RedirectToUUIDView(BrowserView):
         return u''
 
 
-class UUIDTraverser(SimpleHandler):
+@implementer(ITraversable)
+class UUIDTraverser(object):
     "a traversal namespace adapter for traversing to content by its uuid"
+
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
 
     def traverse(self, uuid, remaining):
         "return object with a given uuid, raising NotFound if no such uuid is found"
 
-        obj = uuidToObject(uuid)
-        if not obj:
-            raise LocationError(self.context, uuid)
-        else:
-            return aq_inner(obj)
+        catalog = getToolByName(self.context, 'portal_catalog', None)
+        if catalog is None:
+            raise LocationError(uuid)
+
+        result = catalog.unrestrictedSearchResults(UID=uuid)
+        if len(result) != 1:
+            raise LocationError(uuid)
+
+        brain = result[0]
+
+        # Fix the request so things behave the same as after normal traversal
+        # by replacing the uuid traversal path with normal path
+        # TODO: is there a cleaner way? How to fix request["PARENTS"]?
+        uuid_path = "/++uuid++" + uuid
+        brain_path = brain.getPath()
+        site_path = '/'.join(self.context.getPhysicalPath())
+        obj_path = brain_path[len(site_path):]
+        for rvar in ("ACTUAL_URL" ,"URL", "PATH_INFO", "PATH_TRANSLATED"):
+           self.request[rvar] = self.request[rvar].replace(uuid_path, obj_path)
+
+        obj = brain._unrestrictedGetObject()
+        return aq_inner(obj)
