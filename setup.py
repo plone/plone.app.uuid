c344bcb500f67606ff4f9f126963cf0369d55d57
Gil Forcada Codinachs <gil.gnome@gmail.com>
2018-11-10 21:47:45 +0100

    Update trove classifiers
    [ci-skip]

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='plone uuid',
     author='Martin Aspeli',
